b86ad963dfa53723d379eb41d38b3b26736faf56
yuunp <pacanzachristopher@gmail.com>
2026-02-19 23:22:28 +0800

    added colors for main containers

--- frontend/src/code-page.jsx
+++ frontend/src/code-page.jsx
@@ -2,8 +2,8 @@ import "./main.css";
 
 const CodePage = () => {
   return (
-    <>
-      <div className="header">
+    <div class="parent-container w-full h-screen bg-[#2B303A]">
+      <div className="header w-full h-20 bg-[#D64933]">
         <div className="logo">
           <div className="symbol"></div>
           <div className="text"></div>
@@ -11,18 +11,18 @@ const CodePage = () => {
         <div className="settings"></div>
       </div>
 
-      <div className="main">
-        <div className="code-space">
+      <div className="main w-[100%] h-[80%] flex flex-row justify-around items-center">
+        <div className="code-space w-[65%] h-[100%] bg-red-500">
           <div className="code-space-header">
             <button className="language-picker"></button>
             <input className="code-link" type="text" />
           </div>
-          <div className="code-editor font-bold text-red-900">HELLO</div>
+          <div className="code-editor w-[100%] h-[80%] bg-white"></div>
         </div>
 
-        <div className="version-history">
+        <div className="version-history h-[80%] w-[25%] bg-red-500">
           <button className="save-button"></button>
-          <div className="all-versions">
+          <div className="all-versions h-[100%] w-[100%] bg-[#7C7C7C]">
             <div className="version-1">
               <div className="version-name"></div>
               <div className="created-date"></div>
@@ -30,7 +30,7 @@ const CodePage = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
