3673bb387554d34b976b97e36a63ca91f2c2c33d
yuunp <pacanzachristopher@gmail.com>
2026-02-20 22:42:35 +0800

    layout and colors are done

--- frontend/src/code-page.jsx
+++ frontend/src/code-page.jsx
@@ -2,8 +2,8 @@ import "./main.css";
 
 const CodePage = () => {
   return (
-    <div class="parent-container w-full h-screen bg-[#2B303A]">
-      <div className="header w-full h-20 bg-[#D64933]">
+    <div class="parent-container w-screen h-screen bg-[#2B303A]">
+      <div className="header w-full h-[7%] bg-[#D64933] mb-5">
         <div className="logo">
           <div className="symbol"></div>
           <div className="text"></div>
@@ -11,18 +11,25 @@ const CodePage = () => {
         <div className="settings"></div>
       </div>
 
-      <div className="main w-[100%] h-[80%] flex flex-row justify-around items-center">
-        <div className="code-space w-[65%] h-[100%] bg-red-500">
-          <div className="code-space-header">
-            <button className="language-picker"></button>
-            <input className="code-link" type="text" />
+      <div className="main  flex flex-row justify-around items-center p-3 h-[90%] w-[100%]">
+        <div className="code-space h-[100%] w-[70%]">
+          <div className="code-space-header m-2 h-[5%] w-[100%] flex gap-[2rem]">
+            <button className="language-picker bg-[#EEE5E9] rounded-[50px] h-full w-[100px]"></button>
+            <input
+              className="code-link bg-[#D8D8D8] rounded-[50px] h-full w-[500px]"
+              type="text"
+            />
           </div>
-          <div className="code-editor w-[100%] h-[80%] bg-white"></div>
+          <div className="code-editor  bg-white  rounded-[15px] h-[90%] w-[100%]"></div>
         </div>
 
-        <div className="version-history h-[80%] w-[25%] bg-red-500">
-          <button className="save-button"></button>
-          <div className="all-versions h-[100%] w-[100%] bg-[#7C7C7C]">
+        <div className="version-history h-[100%] w-[20%]">
+          <div className="button-container h-[5%] m-2 flex justify-end">
+            <button className="save-button bg-[#EEE5E9] rounded-[50px] h-[35px] w-[100px]">
+              SAVE
+            </button>
+          </div>
+          <div className="all-versions bg-[#7C7C7C] rounded-[15px] h-[90%]">
             <div className="version-1">
               <div className="version-name"></div>
               <div className="created-date"></div>
