c059eca2cb494401a9cd39147275309b7c23f086
yuunp <pacanzachristopher@gmail.com>
2026-02-22 23:07:16 +0800

    added the drop down menu

--- frontend/src/code-page.jsx
+++ frontend/src/code-page.jsx
@@ -12,22 +12,22 @@ const CodePage = () => {
         <div className="settings"></div>
       </div>
 
-      <div className="main  flex flex-row justify-around items-center p-3 h-[90%] w-[100%]">
-        <div className="code-space h-[100%] w-[70%]">
-          <div className="code-space-header m-2 h-[5%] w-[100%] flex gap-[2rem]">
+      <div className="main  flex flex-row justify-around items-center p-3 h-[90%] w-full">
+        <div className="code-space h-full w-[70%]">
+          <div className="code-space-header m-2 h-[5%] w-full flex gap-8">
             <DropDownMenu />
             <input
-              className="code-link bg-[#D8D8D8] rounded-[50px] h-full p-4 w-[500px] text-gray-500 jersey-25-regular"
+              className="code-link bg-[#D8D8D8] rounded-[50px] h-full p-4 w-125 text-gray-500 jersey-25-regular"
               type="text"
               value={"Type something for the link to appear..."}
             />
           </div>
-          <div className="code-editor  bg-white  rounded-[15px] h-[90%] w-[100%]"></div>
+          <div className="code-editor  bg-white  rounded-[15px] h-[90%] w-full"></div>
         </div>
 
-        <div className="version-history h-[100%] w-[20%]">
+        <div className="version-history h-full w-[20%]">
           <div className="button-container h-[5%] m-2 flex justify-end">
-            <button className="save-button bg-[#EEE5E9] rounded-[50px] h-[35px] w-[100px] jersey-25-regular tracking-widest">
+            <button className="save-button bg-[#EEE5E9] rounded-[50px] h-8.75 w-25 jersey-25-regular tracking-widest">
               SAVE
             </button>
           </div>
--- frontend/src/drop-down-menu.jsx
+++ frontend/src/drop-down-menu.jsx
@@ -4,9 +4,32 @@ import { useState } from "react";
 
 const DropDownMenu = () => {
   const [activateMenu, setActivateMenu] = useState(false);
+  const programmingLanguages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C",
+    "C++",
+    "C#",
+    "TypeScript",
+    "Ruby",
+    "PHP",
+    "Swift",
+    "Kotlin",
+    "Go",
+    "Rust",
+    "R",
+    "Scala",
+    "Dart",
+    "Perl",
+    "Haskell",
+    "Lua",
+    "MATLAB",
+  ];
+
   return (
     <>
-      <div className="language-picker bg-[#EEE5E9] rounded-[50px] pr-1 pl-1 h-full w-[100px] pl-2  jersey-25-regular flex items-center justify-around h-full">
+      <div className="language-picker bg-[#EEE5E9] rounded-[50px] pr-1  h-full w-25 pl-2  jersey-25-regular flex items-center justify-around relative">
         <p>Language</p>
         <button
           className="cursor-pointer"
@@ -14,6 +37,79 @@ const DropDownMenu = () => {
         >
           <IoMdArrowDropdown />
         </button>
+        {activateMenu ? (
+          <div className="menu w-25 h-125 absolute bg-white left-0 top-full border border-black rounded-[10px] flex flex-col overflow-y-auto">
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              C++
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Python
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+            <button className="w-full h-7.5 hover:bg-gray-300 rounded-[10px] cursor-pointer">
+              Java
+            </button>
+          </div>
+        ) : null}
       </div>
     </>
   );
