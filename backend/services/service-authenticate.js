8a066c5f6982e7503a8feccb8059a0fea87cb757
yuunp <pacanzachristopher@gmail.com>
2026-06-16 16:52:29 +0800

    (feat): implemented registration forms. Everytime the user registers details will be saved

--- backend/services/service-authenticate.js
+++ backend/services/service-authenticate.js
@@ -1,19 +1,19 @@
-import { hashPassword } from "../utils/crypto-utils.js";
-
+import { hashPassword, generateUUID } from "../utils/crypto-utils.js";
+import { db } from "../config/db.js";
 
 export const addUserAccount = async (email, password) => {
-    try {
 
+    try {
+        const userID = generateUUID();
         const hashedPassword = await hashPassword(password);
-        console.log(email);
-        console.log(password);
-        console.log(hashedPassword);
+        const insertQueryResult = await db.query("INSERT INTO USER_INFO (user_id, email, password) VALUES($1, $2, $3)", [userID, email, hashedPassword]);
 
 
         return;
 
     }
     catch (error) {
-        console.error(error);
+        console.error("service", error);
     }
 };
+
--- backend/utils/crypto-utils.js
+++ backend/utils/crypto-utils.js
@@ -2,13 +2,15 @@ import { createHash } from "crypto";
 import bcrypt from "bcrypt";
 
 export const hashSourceCode = (sourceCode) => createHash("sha256").update(sourceCode).digest("hex");
+
 export const hashPassword = async (password) => {
     const saltRounds = 12;
 
-
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
+};
 
-
+export const generateUUID = () => {
+    return crypto.randomUUID();
 };
 
