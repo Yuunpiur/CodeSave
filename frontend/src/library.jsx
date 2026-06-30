f603d6f1b92efb7f16e40756a0a8facd4369b5ff
yuunp <pacanzachristopher@gmail.com>
2026-06-30 23:26:38 +0800

    added a header, same header from code-page.jsx

--- frontend/src/library.jsx
+++ frontend/src/library.jsx
@@ -10,7 +10,28 @@ const Library = () => {
     console.log(accessToken);
   }, [accessToken]);
 
-  return <div className="parent-container bg-[#FFFFFF]"></div>;
+  return (
+    <div className="parent-container bg-[#FFFFFF]">
+      <div className="header w-full h-[8%]  bg-[#fdfdfd] flex items-center justify-between px-4 ">
+        <div className="logo font-noto text-[30px] md:text-[40px] text-[#252525] tracking-[0.18em] uppercase ">
+          CodeSave
+        </div>
+        <div className="auth-button-group">
+          <button
+            onClick={() => {
+              navigate("/login");
+            }}
+            className="font-noto text-sm text-white bg-[#252525] border border-[#252525] p-1.5 me-5 rounded-md w-20 cursor-pointer hover:bg-[#ffb522] hover:border-[#ffb522] transition-all duration-150"
+          >
+            Sign Up
+          </button>
+          <button className="font-noto text-sm text-white bg-[#252525] border border-[#252525] p-1.5 rounded-md w-20 cursor-pointer hover:bg-[#ffb522] hover:border-[#ffb522] transition-all duration-150">
+            Log In
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Library;
