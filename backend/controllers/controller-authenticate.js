d35877f48b8e0c1eff892e54b38b6d015888840f
yuunp <pacanzachristopher@gmail.com>
2026-08-22 10:40:21 +0800

    modified the auth.md documentation to something that is easy to read for contributors

--- backend/controllers/controller-authenticate.js
+++ backend/controllers/controller-authenticate.js
@@ -5,7 +5,7 @@ export const signUpController = async (req, res) => {
     try {
         const { username, password } = req.body;
         const { accessToken, refreshToken } = await authenticationServices.signUp(username, password);
-        res.cookie("refreshToken", refreshToken, { httpOnly: false, secure: true, sameSite: "lax" }).json({ accessToken: accessToken }); /* set sameSite to strict on prod */
+        res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true, sameSite: "lax" }).json({ accessToken: accessToken }); /* set sameSite to strict on prod */
     }
     catch (error) {
         console.error(error);
